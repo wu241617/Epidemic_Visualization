55773567c15d0423a2ae94a09bb3752e0066a195
wu241617 <1272689028@qq.com>
2021-04-23 16:39:18 +0800

    fix:项目标题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        projectTitle: '疫 情 可 视 化 信 息 数 据 分 析 平 台',
+        projectTitle: '疫 情 监 测 分 析 可 视 化 系 统',
         projectTime: '系 统 当 前 时 间 ：',
         itemArr: ['数据大屏展示', '中国疫情地图', '世界疫情地图', '全球各国数据', '中国各省数据', '中国城市数据', '疫情推送信息'],
         imgFailure: '图片加载失败',
